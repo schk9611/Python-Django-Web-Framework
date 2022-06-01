09b5a3145c1bc04953a2e2e36f026b2d5e1cd6e4
Sony <schk9611@gmail.com>
2022-06-01 14:24:26 +0900

    update Routing

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Welcome!')
+
+
+def create(request):
+    return HttpResponse('Create')
+
+
+def read(request, id):
+    return HttpResponse('Read!'+id)
