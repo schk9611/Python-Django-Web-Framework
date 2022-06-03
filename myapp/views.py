97a0410c9979774ed5a1291a8493cbff3e397b32
Sony <schk9611@gmail.com>
2022-06-03 13:03:07 +0900

    update views.py add create

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 
+nextId = 4
 topics = [
     {'id': 1, 'title': 'routing', 'body': 'Routing is ...'},
     {'id': 2, 'title': 'view', 'body': 'View is ...'},
@@ -21,6 +23,9 @@ def HTMLTemplate(articleTag):
             {ol}
         </ol>
         {articleTag}
+        <ul>
+            <li><a href="/create/">create</a></li>
+        </ul>
     </body>
     </html>
     '''
@@ -43,5 +48,23 @@ def read(request, id):
     return HttpResponse(HTMLTemplate(article))
 
 
+@csrf_exempt
 def create(request):
-    return HttpResponse('Create')
+    global nextId
+    if request.method == 'GET':
+        article = '''
+            <form action="/create/" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {"id": nextId, "title": title, "body": body}
+        topics.append(newTopic)
+        url = '/read/' + str(nextId)
+        nextId = nextId + 1
+        return redirect(url)
